c6f773f95e8fbd77c7185a60b1bc3c62aec971fb
umaissidd1 <umaissiddiqui83@gmail.com>
2025-03-15 15:03:39 +0500

    API FUNC

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -4,4 +4,12 @@ const Feature = () => {
   return <div>Feature</div>;
 };
 
+//create a function for api call:
+
+const fetchData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
 export default Feature;
